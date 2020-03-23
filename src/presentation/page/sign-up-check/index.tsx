2ca7c4a9271886f0b367124cd80fc70a71761aea
GiPyoo <a01054394111@gmail.com>
2020-03-23 19:36:10 +0900

    이메일 인증 페이지 ui 및 인터렉션 구현
    사용자가 인증번호를 입력할 때 더 직관적일 수 있게 구현함
    Epic #15
    Issue #16

---
+++ src/presentation/page/sign-up-check/index.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import styled from "./styled";
+export const SignUpCheck: React.FC = () => {
+  const nextInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const index = Number(event.currentTarget.id.split("-")[1]);
+    const key = event.keyCode;
+    if ((key === 8 || key === 46) && index !== 0) {
+      const element = document.getElementById(`code-${index - 1}`);
+      element?.focus();
+      return;
+    }
+    if (key === 8 || key === 46) {
+      event.stopPropagation();
+      return;
+    }
+    if (key === 0 || key === 9) {
+      event.stopPropagation();
+      return;
+    }
+    if (/[^a-zA-Z0-9]/.test(event.key)) {
+      event.currentTarget.value = event.currentTarget.value
+        .replace(/[^a-zA-Z0-9]/gi, "")
+        .trim();
+      return;
+    }
+    if (index === 6) event.currentTarget.blur();
+    const element = document.getElementById(`code-${index + 1}`);
+    element?.focus();
+  };
+
+  const blockKorean = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.value = event.target.value
+      .replace(/[^a-zA-Z0-9]/gi, "")
+      .trim();
+  };
+
+  const onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
+    event.target.value = "";
+  };
+
+  return (
+    <styled.SignUpCheckBox>
+      <styled.Span>
+        가입하신 이메일을 통해 인증번호 6자리를 보냈습니다. 다음 입력칸에 알맞은
+        인증번호를 입력하시면 가입이 완료됩니다.
+      </styled.Span>
+      <styled.NumberCodeInputBox>
+        {Array.from({ length: 6 }, (element, index) => (
+          <styled.NumberCodeInput
+            maxLength={1}
+            key={index}
+            id={`code-${index}`}
+            onFocus={onFocus}
+            onBlur={blockKorean}
+            onKeyUp={nextInput}
+          ></styled.NumberCodeInput>
+        ))}
+      </styled.NumberCodeInputBox>
+    </styled.SignUpCheckBox>
+  );
+};
