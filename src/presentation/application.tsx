0fbd51b702562e9c4b7008ee308a014e6b880f9c
GiPyoo <a01054394111@gmail.com>
2020-03-23 20:03:46 +0900

    회원가입과 ui 연결
    Epic #15
    Issue #18 #15 #8

--- src/presentation/application.tsx
+++ src/presentation/application.tsx
@@ -19,6 +19,7 @@ function App() {
           <Route path="/">
             <Login></Login>
           </Route>
+          <Route path="/codecheck"></Route>
         </Switch>
       </section>
     </Router>
