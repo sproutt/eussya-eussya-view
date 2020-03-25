f62542df4a6b0c83496726f10eb1200abb2aa67f
GiPyoo <a01054394111@gmail.com>
2020-03-25 20:38:06 +0900

    이메일 코드 인증 화면 인터랙션 코드 리팩토링
    Issue #27

--- src/presentation/page/sign-up-check/index.tsx
+++ src/presentation/page/sign-up-check/index.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
 import styled from "./styled";
+import { Application } from "context-instance";
+import { useParams } from "react-router-dom";
 export const SignUpCheck: React.FC = () => {
+  const params = useParams<{ email: string }>();
+  const [code, setCode] = React.useState<string[]>(new Array(8));
+
   const nextInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
     const index = Number(event.currentTarget.id.split("-")[1]);
     const key = event.keyCode;
@@ -9,17 +14,14 @@ export const SignUpCheck: React.FC = () => {
       element?.focus();
       return;
     }
-    if (key === 8 || key === 46 || key === 0 || key === 9) {
+    if (key === 8 || key === 46 || key === 0 || key === 9 || key === 20) {
       event.stopPropagation();
       return;
     }
-    if (/[^a-zA-Z0-9]/.test(event.key)) {
-      event.currentTarget.value = event.currentTarget.value
-        .replace(/[^a-zA-Z0-9]/gi, "")
-        .trim();
-      return;
+    if (index === 7) {
+      event.currentTarget.blur();
+      Application.services.member.checkCode(params.email, code.join(""));
     }
-    if (index === 7) event.currentTarget.blur();
     if (!event.currentTarget.value.length) return;
     const element = document.getElementById(`code-${index + 1}`);
     element?.focus();
@@ -33,6 +35,17 @@ export const SignUpCheck: React.FC = () => {
 
   const onFocus = (event: React.FocusEvent<HTMLInputElement>) => {
     event.target.value = "";
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = Number(event.currentTarget.id.split("-")[1]);
+    console.log(code);
+    if (/[^a-zA-Z0-9]/.test(event.target.value)) {
+      event.target.value = "";
+      return;
+    }
+    code[index] = event.target.value;
+    setCode(code);
   };
 
   return (
@@ -50,6 +63,7 @@ export const SignUpCheck: React.FC = () => {
             onFocus={onFocus}
             onBlur={blockKorean}
             onKeyUp={nextInput}
+            onChange={onChange}
           ></styled.NumberCodeInput>
         ))}
       </styled.NumberCodeInputBox>
