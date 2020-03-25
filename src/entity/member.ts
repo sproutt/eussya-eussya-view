ca7ff3baf2db4a1e5e7199eaf88b4bcfa8b74e99
GiPyoo <a01054394111@gmail.com>
2020-03-25 20:42:27 +0900

    메일 보내기 요청이 이메일 인증 코드 보내는 요청으로 바뀌면서 관련된 부분 수정
    Isuue #27

--- src/entity/member.ts
+++ src/entity/member.ts
@@ -1,7 +1,7 @@
 import { Entity } from "./index";
 export class Member implements Entity {
   constructor(
-    public readonly email?: string,
+    public readonly memberId?: string,
     public readonly nickName?: string,
     public readonly password?: string
   ) {}
