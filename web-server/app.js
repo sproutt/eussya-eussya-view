7b646b44ac0853c70b5e41c3c0512e3fb511fcc5
GiPyoo <a01054394111@gmail.com>
2020-04-06 20:09:46 +0900

    web server 버그 수정
    Issue #35

--- web-server/app.js
+++ web-server/app.js
@@ -14,15 +14,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("*", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
