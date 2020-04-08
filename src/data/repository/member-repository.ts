36eeec5867f62b13d4edd4295ebdf0889a55d988
GiPyoo <a01054394111@gmail.com>
2020-04-08 21:08:47 +0900

    토큰 통신을 할 때 헤더 authorization 대싱 Authorization으로 수정
    서버사이드에서 보내주는 헤더 이름과 통일하기 위해서
    
    Issue #40

--- src/data/repository/member-repository.ts
+++ src/data/repository/member-repository.ts
@@ -32,8 +32,8 @@ export class MemberRepository implements MemberRepositoryImpl {
   async login(member: Member) {
     try {
       const result = await this.api.login(member);
-      if (!result.headers["authorization"]) return false;
-      this.storage.set(result.headers["authorization"]);
+      if (!result.headers["Authorization"]) return false;
+      this.storage.set(result.headers["Authorization"]);
       return result.status === HttpStatus.OK;
     } catch (error) {
       return false;
