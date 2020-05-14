174b7e30ef051bafff536b444e4ca85713339020
GiPyoo <a01054394111@gmail.com>
2020-05-14 10:41:32 +0900

    나중에 추가할 팝업 내용이 들어갈 컴포넌트 생성
    issue#54

---
+++ src/presentation/molecule/early-crazy-pop-up/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import styled from "./styled";
+export const EarlyCrazyPopUp: React.FC = () => {
+  return <styled.PopUpWriter></styled.PopUpWriter>;
+};
---
+++ src/presentation/molecule/early-crazy-pop-up/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const PopUpWriter = styled.article`
+  width: 100%;
+  height: 100%;
+`;
+
+export default { PopUpWriter };
