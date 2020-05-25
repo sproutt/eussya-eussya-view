4503e9ff541cd3f856162bcea8be0d87984a322b
GiPyoo <a01054394111@gmail.com>
2020-05-25 19:10:51 +0900

    동기부여 글귀 요청 api 구현

--- src/@context/repositories/index.tsx
+++ src/@context/repositories/index.tsx
@@ -1,10 +1,12 @@
 import { HttpProviderDependencies } from "@context/http-providers";
 import { StorageProviderDependencies } from "@context/storage-providers";
 import { MemberRepositoryImpl } from "data/repository/member-repository-impl";
+import SystemRopositoryImpl from "data/repository/system-repositort-impl";
 
 //레포지토리 의존성을 주입하는 곳
 export class RepositoryContainer {
   public readonly memberRepository: MemberRepositoryImpl;
+  public readonly systemRepository: SystemRopositoryImpl;
   constructor(
     apiProviders: HttpProviderDependencies,
     storageProviders: StorageProviderDependencies
---
+++ src/data/http/system-api.ts
@@ -0,0 +1,20 @@
+import { AxiosWrapper } from "./axios/axios-wrapper";
+import { AxiosResponse } from "axios";
+class SystemApiProvider {
+  private axiosWrapper: AxiosWrapper;
+  constructor() {
+    this.axiosWrapper = new AxiosWrapper();
+  }
+
+  async getMotivatedText(): Promise<AxiosResponse> {
+    try {
+      return await this.axiosWrapper
+        .getAxios()
+        .get(process.env.REACT_APP_HOST + "/phrase");
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default SystemApiProvider;
