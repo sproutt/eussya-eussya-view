a8e39b02b1a1d71d64a5fb0b8aefd58609138901
GiPyoo <a01054394111@gmail.com>
2020-05-25 19:13:42 +0900

    시스템 서비스 의존성 연결

--- src/@context/repositories/index.tsx
+++ src/@context/repositories/index.tsx
@@ -1,7 +1,7 @@
 import { HttpProviderDependencies } from "@context/http-providers";
 import { StorageProviderDependencies } from "@context/storage-providers";
 import { MemberRepositoryImpl } from "data/repository/member-repository-impl";
-import SystemRopositoryImpl from "data/repository/system-repositort-impl";
+import { SystemRopositoryImpl } from "data/repository/system-repositort-impl";
 
 //레포지토리 의존성을 주입하는 곳
 export class RepositoryContainer {
@@ -15,5 +15,6 @@ export class RepositoryContainer {
       apiProviders.memberApi,
       storageProviders.JWTTokenStorage
     );
+    this.systemRepository = new SystemRopositoryImpl(apiProviders.systemApi);
   }
 }
--- src/@context/services/index.ts
+++ src/@context/services/index.ts
@@ -1,10 +1,13 @@
 import { RepositoryContainer } from "@context/repositories";
 import { MemberService } from "core/service/member-service";
+import SystemService from "core/service/system-service";
 
 //서비스 의존성 주입
 export class ServiceDependencies {
   public readonly member: MemberService;
+  public readonly system: SystemService;
   constructor(repositories: RepositoryContainer) {
     this.member = new MemberService(repositories.memberRepository);
+    this.system = new SystemService(repositories.systemRepository);
   }
 }
