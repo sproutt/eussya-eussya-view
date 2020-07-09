f65dbe9ad9a0aea275242125fce9eff1b6c6ddc4
GiPyoo <a01054394111@gmail.com>
2020-07-09 18:06:42 +0900

    add custom style
    size, color and so on...
    
    Issue #62

--- src/utils/style/color.ts
+++ src/utils/style/color.ts
@@ -1,6 +1,6 @@
 export enum Color {
-  MAIN_COLOR = "#274c5e",
+  MAIN_COLOR = "#0071e2",
   DISABLED_MAIN_COLOR = "#1a323e",
   DISABLED_COLOR = "#6d6d6d",
-  WARNING_COLOR = "#e20000"
+  WARNING_COLOR = "#e20000",
 }
