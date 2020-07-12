3d1234c5e5635c3ce2f3110c1134219053209a54
GiPyoo <a01054394111@gmail.com>
2020-07-13 05:48:14 +0900

    Inject dependency about mission
    Issue #65

--- src/@context/repositories/index.tsx
+++ src/@context/repositories/index.tsx
@@ -2,11 +2,13 @@ import { HttpProviderDependencies } from "@context/http-providers";
 import { StorageProviderDependencies } from "@context/storage-providers";
 import { MemberRepositoryImpl } from "data/repository/member-repository-impl";
 import { SystemRopositoryImpl } from "data/repository/system-repositort-impl";
+import MissonRepositoryImpl from "data/repository/mission-repository-impl";
 
 //레포지토리 의존성을 주입하는 곳
 export class RepositoryContainer {
   public readonly memberRepository: MemberRepositoryImpl;
   public readonly systemRepository: SystemRopositoryImpl;
+  public readonly missionRepository: MissonRepositoryImpl;
   constructor(
     apiProviders: HttpProviderDependencies,
     storageProviders: StorageProviderDependencies
@@ -16,5 +18,6 @@ export class RepositoryContainer {
       storageProviders.JWTTokenStorage
     );
     this.systemRepository = new SystemRopositoryImpl(apiProviders.systemApi);
+    this.missionRepository = new MissonRepositoryImpl(apiProviders.missionApi);
   }
 }
