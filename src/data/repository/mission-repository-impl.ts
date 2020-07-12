3dd1b5f0d9b67f15eaa934ec5035da3b9cb5b70d
GiPyoo <a01054394111@gmail.com>
2020-07-13 05:47:30 +0900

    Impliment mission repository about post mission
    Issue #65

---
+++ src/data/repository/mission-repository-impl.ts
@@ -0,0 +1,37 @@
+import { OK } from "http-status-codes";
+import { PublicErrorMessage } from "./../../enum/public-error-message";
+import { MissionErrorMessage } from "./../../enum/mission-error-message";
+import Mission from "core/entity/mission";
+import MissionRepository from "core/use-case/mission-repository";
+import MissionApiProvider from "data/http/mission.api";
+import RepoResponseType from "data/response-type/repo-response";
+import { MissionErrorCode } from "enum/mission-error-code";
+
+export default class MissonRepositoryImpl implements MissionRepository {
+  constructor(private api: MissionApiProvider) {}
+
+  async postMission(mission: Mission) {
+    try {
+      let result = await this.api.postMisson(mission);
+      return new RepoResponseType<undefined>(result.status === OK, "");
+    } catch (error) {
+      if (!(error && error.response)) throw error;
+      if (error.response.data.errorCode === undefined)
+        return new RepoResponseType<undefined>(
+          false,
+          PublicErrorMessage.REQUEST_FAIL
+        );
+      if (error.response.data.errorCode === MissionErrorCode.NOT_DAWN)
+        return new RepoResponseType<undefined>(
+          false,
+          MissionErrorMessage.NOT_DAWN
+        );
+      if (error.response.data.errorCode === MissionErrorCode.NOT_AUTH_USER)
+        return new RepoResponseType<undefined>(
+          false,
+          MissionErrorMessage.NOT_AUTH_USER
+        );
+      throw error;
+    }
+  }
+}
