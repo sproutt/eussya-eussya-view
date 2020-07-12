ba01a580477a877ec5373ce179aeeb9820f5d262
GiPyoo <a01054394111@gmail.com>
2020-07-13 05:42:07 +0900

    add error code enum and error message enum
    To take appropriate action for various error situations
    
    Issue #65

---
+++ src/enum/mission-error-code.ts
@@ -0,0 +1,4 @@
+export enum MissionErrorCode {
+  NOT_DAWN = "MIS001",
+  NOT_AUTH_USER = "MIS002",
+}
---
+++ src/enum/public-error-message.ts
@@ -0,0 +1,3 @@
+export enum PublicErrorMessage {
+  REQUEST_FAIL = "죄송합니다, 요청을 불러오는데 실패했습니다",
+}
