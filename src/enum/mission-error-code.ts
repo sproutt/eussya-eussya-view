76d05eaa2ca70d6bca6b598d320b60b0741bb05d
GiPyoo <a01054394111@gmail.com>
2020-07-17 06:55:04 +0900

    add error code and message
    요청시 각각의 다른 오류들을 구분하여 다른 액션을 하기 위해서
    
    Issue #65

--- src/enum/mission-error-code.ts
+++ src/enum/mission-error-code.ts
@@ -1,4 +1,3 @@
 export enum MissionErrorCode {
   NOT_DAWN = "MIS001",
-  NOT_AUTH_USER = "MIS002",
 }
--- src/enum/mission-error-message.ts
+++ src/enum/mission-error-message.ts
@@ -1,4 +1,3 @@
 export enum MissionErrorMessage {
   NOT_DAWN = "입력한 시간이 유효하지 않습니다",
-  NOT_AUTH_USER = "인증 오류",
 }
