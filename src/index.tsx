ec39ee2406f68b03479e1afee8046553f257c465
GiPyoo <a01054394111@gmail.com>
2020-07-23 10:29:04 +0900

    add context provider component in root
    만들어논 컨텍스트 api를 앱 내에서 사용하기 위해, 인증관 련 컨텍스트이기 때문
    
    Issue #67

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./presentation/application";
 import * as serviceWorker from "./service-worker";
 import "./index.scss";
+import { AuthContextProvider } from "context-api/context/auth-context";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
