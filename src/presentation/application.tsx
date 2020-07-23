4011dcd267331f026837e5433f7c6a7692516154
GiPyoo <a01054394111@gmail.com>
2020-07-23 10:30:52 +0900

    인증상태에 따른 네비게이션 바 구현
    Issue #67

--- src/presentation/application.tsx
+++ src/presentation/application.tsx
@@ -4,14 +4,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./application.scss";
 import { SignUpCheck } from "./page/sign-up-check";
 import { Main } from "./page/main";
+import { Login } from "./page/login";
+import { NavBar } from "./molecule/nav-bar";
 
 function App() {
   return (
     <Router>
       <section id="main-section">
+        <NavBar></NavBar>
         <Switch>
           <Route path="/signup">
             <SignUp></SignUp>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
           </Route>
           <Route path="/codecheck/:email">
             <SignUpCheck></SignUpCheck>
