c20df47b58229209f450feb6f54c0405ee4b2c73
GiPyoo <a01054394111@gmail.com>
2021-01-17 23:22:30 +0900

    fix: service worker version

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./presentation/application";
-import * as serviceWorker from "./service-worker";
 import "./index.scss";
 import { AuthContextProvider } from "context-api/context/auth-context";
 
@@ -11,8 +10,3 @@ ReactDOM.render(
   </AuthContextProvider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
