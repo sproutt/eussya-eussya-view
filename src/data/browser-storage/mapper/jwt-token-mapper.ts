978d6b5c4484dcfa7f5e5d64f40320dc0e222a07
GiPyoo <a01054394111@gmail.com>
2021-01-18 19:27:03 +0900

    hotfix error

--- src/data/browser-storage/mapper/jwt-token-mapper.ts
+++ src/data/browser-storage/mapper/jwt-token-mapper.ts
@@ -3,11 +3,15 @@ import { BrowserStorageMapperImpl } from "./../browser-storage-mapper";
 
 export class JWTTokenMapper implements BrowserStorageMapperImpl<AccessToken> {
   toJson(target: AccessToken): string {
-    return JSON.stringify({accessToken : target.accessToken!,refreshToken: target.refreshToken!});
+    return JSON.stringify({
+      accessToken: target.accessToken!,
+      refreshToken: target.refreshToken!,
+    });
   }
 
   fromJson(jsonString: string): AccessToken {
-    let json =  JSON.parse(jsonString)
+    let json = JSON.parse(jsonString);
+    if (!json) return new AccessToken("", "");
     return new AccessToken(json.accessToken, json.refreshToken);
   }
 }
