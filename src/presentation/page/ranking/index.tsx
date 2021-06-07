237ff87102a26af4cca87fac506e1255526b67cb
spiderq2697 <spiderq10@gmail.com>
2021-06-07 18:04:54 +0900

    index.tsx에 useState 적용

--- src/presentation/page/ranking/index.tsx
+++ src/presentation/page/ranking/index.tsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Filter from "./Filter";
 import MyRankingbox from "./MyRankingbox";
 import Rankingbox from "./Rankingbox";
 import Content from "./Content";
 import styled from "./styled";
 
-const week = `3월 둘째주(14~18)`;
-
-const users = [
+let users = [
   {
     ranking: 1,
     trend: "⬆",
@@ -24,7 +22,7 @@ const users = [
   },
 ];
 
-const myLanking = {
+let myLanking = {
   ranking: 12,
   trend: "⬆",
   id: "egoing",
@@ -32,25 +30,31 @@ const myLanking = {
   totalCount: 8,
 };
 
+const THIS_WEEK = `3월 둘째주(14~18)`;
+
 export const Ranking: React.FC = () => {
+  const [userRanking, setUserRanking] = useState(users);
+  const [week, setWeek] = useState(THIS_WEEK);
+  const [myCurrentRanking, setMyCurrentLanking] = useState(myLanking);
+
   return (
     <styled.Ranking>
       <Filter week={week} />
       <Content />
       <MyRankingbox
-        ranking={myLanking.ranking}
-        trend={myLanking.trend}
-        id={myLanking.id}
-        count={myLanking.count}
-        totalCount={myLanking.totalCount}
+        ranking={myCurrentRanking.ranking}
+        trend={myCurrentRanking.trend}
+        id={myCurrentRanking.id}
+        count={myCurrentRanking.count}
+        totalCount={myCurrentRanking.totalCount}
       />
-      {users.map((user) => (
+      {users.map((userRanking) => (
         <Rankingbox
-          ranking={user.ranking}
-          trend={user.trend}
-          id={user.id}
-          count={user.count}
-          totalCount={user.totalCount}
+          ranking={userRanking.ranking}
+          trend={userRanking.trend}
+          id={userRanking.id}
+          count={userRanking.count}
+          totalCount={userRanking.totalCount}
         />
       ))}
     </styled.Ranking>
